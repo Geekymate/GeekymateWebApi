a4440e162f694496446972979e1e89e9031947b6
iGilga <pyua87@gmail.com>
2016-07-09 20:00:50 +0300

    Добавлен Api к контроллеру тестов.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+
+import {testController} from './components';
+
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -7,7 +10,9 @@ var port = 9000;
 
 app.get('/api/help', function (req, res) {
   res.json({"help": "help"})
-})
+});
+
+app.use('/api/tests', testController());
 
 app.listen(port, function () {
   console.log('App listening on port ' + port);
