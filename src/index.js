3ac957cf3e96837c74ccd99a1cbc8f6a86f8c843
Naukomania <naukomania@gmail.com>
2016-07-10 00:21:16 +0300

    Revert "develop: basic bot polling"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import nodeTelegramBotApi from 'node-telegram-bot-api';
 
 import {testController} from './components';
 
@@ -11,27 +10,6 @@ var port = 9000;
 
 app.get('/api/help', function (req, res) {
   res.json({"help": "help"})
-});
-
-app.get('/bot/polling', function (req, res) {
-  var TelegramBot = require('node-telegram-bot-api');
-  var token = '230686792:AAF42GwXm2ntPWcn1IYKemkt_mBf2QsCQTU';
-	// Setup polling way
-	var request = require('request');
-	var options = {
-	  polling: true
-	};
-	var bot = new TelegramBot(token, options);
-	bot.getMe().then(function (me) {
-	  console.log('Hi my name is %s!', me.username);
-	});
-	bot.onText(/\/msg_info/, function (msg) {
-	  console.log(msg);
-	  bot.sendMessage(msg.chat.id, JSON.stringify(msg));
-	});
-	bot.on('message', function (msg) {
-	  bot.sendMessage(msg.chat.id, 'Что значит ' + msg.text + '?');
-	});
 });
 
 app.use('/api/tests', testController());
