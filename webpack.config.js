222b65a4d7f10ab7971fe32b458194814fc997f0
iGilga <pyua87@gmail.com>
2016-07-09 18:06:55 +0300

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'backend.js'
+  },
+  externals: nodeModules,
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }]
+  }
+}
