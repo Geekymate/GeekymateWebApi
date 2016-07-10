080e7f0bbee79fb6e3ce7327a7defcd7888810f7
iGilga <pyua87@gmail.com>
2016-07-10 09:38:23 +0300

    Расширение апи контроллера отчетность тестов

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import TelegramBot from 'node-telegram-bot-api';
 
-
-import {testController, accountController, publishTestController} from './components';
+import {
+  testController,
+  accountController,
+  publishTestController,
+  reportController
+} from './components';
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 var port = 9000;
 
 app.use(function(req, res, next) {
@@ -20,30 +23,13 @@ app.get('/api/help', (req, res) => {
   res.json({"help": "help"})
 });
 
-// app.get('/bot/polling', (req, res) => {
-//   var token = '230686792:AAF42GwXm2ntPWcn1IYKemkt_mBf2QsCQTU';
-// 	// Setup polling way
-// 	var options = {
-// 	  polling: true
-// 	};
-// 	var bot = new TelegramBot(token, options);
-// 	bot.getMe().then((me) => {
-// 	  console.log('Hi my name is %s!', me.username);
-// 	});
-// 	bot.onText(/\/msg_info/, (msg) => {
-// 	  console.log(msg);
-// 	  bot.sendMessage(msg.chat.id, JSON.stringify(msg));
-// 	});
-// 	bot.on('message', (msg) => {
-// 	  bot.sendMessage(msg.chat.id, 'Что значит тыкать' + msg.text + '?');
-// 	});
-// });
-
 app.use('/api/tests', testController());
 
 app.use('/api/accounts', accountController());
 
 app.use('/api/publish', publishTestController());
+
+app.use('/api/reports', reportController());
 
 app.listen(port, () => {
   console.log('App listening on port ' + port);
