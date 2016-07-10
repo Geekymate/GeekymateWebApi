4b5ed02d9bc2a3a6873452bb389c7b3124d9c58d
iGilga <pyua87@gmail.com>
2016-07-10 06:23:47 +0300

    Разришен апи для публикация теста

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import TelegramBot from 'node-telegram-bot-api';
+// import TelegramBot from 'node-telegram-bot-api';
 
-import {testController, accountController} from './components';
+
+import {testController, accountController, publishTestController} from './components';
 
 var app = express();
 app.use(bodyParser.json());
@@ -19,28 +20,30 @@ app.get('/api/help', (req, res) => {
   res.json({"help": "help"})
 });
 
-app.get('/bot/polling', (req, res) => {
-  var token = '230686792:AAF42GwXm2ntPWcn1IYKemkt_mBf2QsCQTU';
-	// Setup polling way
-	var options = {
-	  polling: true
-	};
-	var bot = new TelegramBot(token, options);
-	bot.getMe().then((me) => {
-	  console.log('Hi my name is %s!', me.username);
-	});
-	bot.onText(/\/msg_info/, (msg) => {
-	  console.log(msg);
-	  bot.sendMessage(msg.chat.id, JSON.stringify(msg));
-	});
-	bot.on('message', (msg) => {
-	  bot.sendMessage(msg.chat.id, 'Что значит тыкать' + msg.text + '?');
-	});
-});
+// app.get('/bot/polling', (req, res) => {
+//   var token = '230686792:AAF42GwXm2ntPWcn1IYKemkt_mBf2QsCQTU';
+// 	// Setup polling way
+// 	var options = {
+// 	  polling: true
+// 	};
+// 	var bot = new TelegramBot(token, options);
+// 	bot.getMe().then((me) => {
+// 	  console.log('Hi my name is %s!', me.username);
+// 	});
+// 	bot.onText(/\/msg_info/, (msg) => {
+// 	  console.log(msg);
+// 	  bot.sendMessage(msg.chat.id, JSON.stringify(msg));
+// 	});
+// 	bot.on('message', (msg) => {
+// 	  bot.sendMessage(msg.chat.id, 'Что значит тыкать' + msg.text + '?');
+// 	});
+// });
 
 app.use('/api/tests', testController());
 
 app.use('/api/accounts', accountController());
+
+app.use('/api/publish', publishTestController());
 
 app.listen(port, () => {
   console.log('App listening on port ' + port);
