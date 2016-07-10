823659bfca1a2524d77a65a6d09057b7bd9154db
iGilga <pyua87@gmail.com>
2016-07-10 07:21:41 +0300

    Удалены лишние логи

--- src/components/publishTest/publishTestController.js
+++ src/components/publishTest/publishTestController.js
@@ -8,14 +8,10 @@ export default function() {
   var testService = new TestService();
   publishTestController.route('/')
     .post((req, res) => {
-      console.log('publishTestController');
       var test = req.body;
-      console.log(test);
       let query = testService.getTest(test.id);
       let promise = query.exec();
       promise.then((test) => {
-        console.log('Get test');
-        console.log(telegramBotService);
         telegramBotService.publish(test);
         res.sendStatus(200);
       });
